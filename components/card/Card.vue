e4f9b07c717dee0fe9fc6cd059fa8697f2706537
meesakveld <meesakveld@gmail.com>
2024-10-18 11:42:26 +0200

    Made avatar img smaller in card

--- components/card/Card.vue
+++ components/card/Card.vue
@@ -36,7 +36,7 @@ withDefaults(defineProps<Props>(), {
         <p v-if="data.synopsis" class="!m-0">{{ data.synopsis }}</p>
         
         <div class="flex flex-wrap gap-y-1 gap-x-4 mt-4 items-center" style="color: var(--vp-c-text-2);">
-          <img v-if="data.author?.avatarUrl" :src="withBase(data.author?.avatarUrl)" :alt="data.author.name" class="rounded-full w-8 h-8" />
+          <img v-if="data.author?.avatarUrl" :src="withBase(data.author?.avatarUrl)" :alt="data.author.name" class="rounded-full w-6 h-6" />
 
           <p v-if="data.author?.name" class="!m-0">{{ data.author.name }}</p>
         
